ad6343167e158b40688ca272251175f17c67a4fc
Pratham Ranjan Singh <67726405+prathamranjansingh@users.noreply.github.com>
2024-06-18 16:20:28 +0530

    Revert "Update StoreContext.jsx"
    This reverts commit ab8b504adddb4a47e8a1623b811a0c6afa904c02.

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useEffect, useState } from "react";
 export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://groceryappbackend-40vw.onrender.com";
+  const url = "http://localhost:3000";
   const [token, setToken] = useState("");
   const [grocery_list, setgroceryList] = useState([]);
 
