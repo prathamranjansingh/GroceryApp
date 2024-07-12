528265b5ac94d2488c176fe7ebc404fdf6ea5db3
Pratham Ranjan Singh <67726405+prathamranjansingh@users.noreply.github.com>
2024-07-12 15:06:20 +0530

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,21 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { Auth0Provider } from "@auth0/auth0-react";
 import StoreContextProvider from "./context/StoreContext.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StoreContextProvider>
-      <Auth0Provider
-        domain="dev-7x12ac2q12g8b3qv.us.auth0.com"
-        clientId="sQeiXOqKTG9vGQnpmkC77QvCF9wTsc0x"
-        authorizationParams={{
-          redirect_uri: window.location.origin,
-        }}>
-        <App />
-      </Auth0Provider>
+      <App />
     </StoreContextProvider>
   </BrowserRouter>
 );
